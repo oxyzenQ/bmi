377a334a76d2a64cbe90e3209a990d08cb678814
oxchin <nightlybot1@gmail.com>
2025-08-04 00:55:38 +0800

    Delete .next directory

--- .next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST='{"pages":{},"app":{"/tmp/apel/BMI-oxy/src/app/layout":["static/media/e4af272ccee01ff0-s.p.woff2"]},"appUsingSizeAdjust":true,"pagesUsingSizeAdjust":false}';
