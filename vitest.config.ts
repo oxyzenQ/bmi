c29fec53c9613b5a32fb98cd6d7d542836fefa7d
rezky_nightky <with.rezky@gmail.com>
2026-03-30 13:31:11 +0700

    fix: vitest svelte preprocessing
    Author: rezky_nightky <with dot rezky at gmail dot com>
    Repository: bmi
    Branch: main
    Signing: GPG (4B65AAC2)
    HashAlgo: BLAKE3
    
    [ Block Metadata ]
    BlockHash: 72472c203acba1b65585ddedcc1cdfd0ab8d923a1f065c0b07a61b51cf22e8e1
    PrevHash: fd56512999f473118d9cd56985f9856f726f636756bb2b96ecec82680253ee43
    PatchHash: f1c0e42575c51b072221892581750b444793b6076cc48992f91e7e5065de6956
    
    FilesChanged: 3
    Lines: +179 / -186
    
    Timestamp: 2026-03-30T06:31:11Z
    Signature1: 3293eba28c1b20330697ac04efe70710492ce013c80842f20919b6a4bbc95d74
    Signature2: 5ebcf84da57d148e2e08b62b86ba737234b1081e9ce2f3568967d82c5988bc6d

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,9 @@
-import { svelte, vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-	plugins: [svelte({ hot: false, preprocess: vitePreprocess() })],
+	// @ts-ignore - Vite version mismatch between vitest and project
+	plugins: [svelte({ hot: false })],
 	test: {
 		environment: 'jsdom',
 		setupFiles: ['./src/test-setup.ts'],
