9e406870a10d7ec992520a2491dfa313110db50b
rezky_nightky <130107241+oxyzenQ@users.noreply.github.com>
2026-05-17 22:10:14 +0700

    fix: formating and ci
    Signed-off-by: rezky_nightky <130107241+oxyzenQ@users.noreply.github.com>
    
    ── bmi/dev · c9dfc45b8044 · 3 file · +18/-4 · 2026-05-17T15:10:14Z

--- svelte.config.js
+++ svelte.config.js
@@ -2,13 +2,17 @@
 import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
+const adapterOptions = process.env.VERCEL_NODE_RUNTIME
+	? { runtime: process.env.VERCEL_NODE_RUNTIME }
+	: {};
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
 	kit: {
-		adapter: adapter(),
+		adapter: adapter(adapterOptions),
 		serviceWorker: {
 			register: false // We register manually for better control
 		}
